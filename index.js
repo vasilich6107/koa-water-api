2fa3cbc049211429bc82d189cb91f784758c59f5
vdytsiak <v.ditsyak@gmail.com>
2018-01-24 15:58:51 +0200

    Develop branch.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = new Koa();
 
 // response
 app.use(ctx => {
-  ctx.body = 'Hello Koa is on Heroku';
+  ctx.body = 'Hello Koa is on Heroku develop branch';
 });
 
 app.listen(process.env.PORT || '3000');
